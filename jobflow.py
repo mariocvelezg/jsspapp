abce9a91ef600c942abd2842d25c198faf78e228
unknown <mariocvelez@gmail.com>
2023-10-28 13:31:47 -0500

    Primer commit

---
+++ jobflow.py
@@ -0,0 +1,51 @@
+import os
+from flask import Flask, request
+from flask.templating import render_template
+from werkzeug.utils import secure_filename
+import reglas
+
+UPLOAD_FOLDER = 'static/files'
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+archivo_t, archivo_r = '', ''
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    
+    if request.method == 'POST':
+        global archivo_t, archivo_r
+        if archivo_t == '' and archivo_t == '':
+            tiempos = request.files["archivo_tiempos"]
+            rutas = request.files["archivo_rutas"]
+        
+            if 'archivo_tiempos' in request.files:
+                # Guardar archivo de tiempos
+                archivo_t = secure_filename(tiempos.filename)
+                tiempos.save(os.path.join(app.config['UPLOAD_FOLDER'], archivo_t))
+                
+            if 'archivo_rutas' in request.files:
+                # Guardar archivo de rutas
+                archivo_r = secure_filename(rutas.filename)
+                rutas.save(os.path.join(app.config['UPLOAD_FOLDER'], archivo_r))
+
+    return render_template("index2.html", archivo_t=archivo_t, archivo_r=archivo_r)
+
+@app.route('/solucion', methods=['GET', 'POST'])
+def muestra_Sol():
+    if request.method == 'POST':
+        global archivo_t, archivo_r
+        tiempos_proceso =  [[float(i) for i in linea.strip().split('\t')] for linea in open(UPLOAD_FOLDER+'/'+archivo_t,'r')]
+        rutas_produccion = [[int(i)-1 for i in linea.strip().split('\t')] for linea in open(UPLOAD_FOLDER+'/'+archivo_r,'r')]
+        regla = request.form['regla']
+        if regla == 'spt':  
+            Cmax, resultado = reglas.spt_rule(tiempos_proceso, rutas_produccion)
+        elif regla == 'mwkr':
+            Cmax, resultado = reglas.mwkr_rule(tiempos_proceso, rutas_produccion)
+        gantt = reglas.imprime_gantt(rutas_produccion, resultado)
+    return render_template("gantt.html", regla=regla)
+
+if __name__ == '__main__':
+    app.run(debug = True)
