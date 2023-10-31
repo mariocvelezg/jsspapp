8040677b571a0a985867307de4c5f44ee666c5e7
unknown <mariocvelez@gmail.com>
2023-10-31 06:54:18 -0500

    borra archivos txt cada 24 hroas

--- jobflow.py
+++ jobflow.py
@@ -1,35 +1,44 @@
 import os
 from flask import Flask, request
 from flask.templating import render_template
+from flask_apscheduler import APScheduler
 from werkzeug.utils import secure_filename
-import plotly.express as px
+#import plotly.express as px
 import reglas
 
 UPLOAD_FOLDER = 'static'
 
 app = Flask(__name__)
+scheduler = APScheduler()
 app.config['SECRET_KEY'] = 'secret'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-archivo_t, archivo_r = '', ''
+def borar_archivos():
+    for document in os.listdir('./static/'):
+        if not document.startswith('style'):
+            os.remove('./static/' + document)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     global archivo_t, archivo_r
     if request.method == 'POST':
-        if archivo_t == '' and archivo_t == '':
-            tiempos = request.files["archivo_tiempos"]
-            rutas = request.files["archivo_rutas"]
         
-            if 'archivo_tiempos' in request.files:
-                # Guardar archivo de tiempos
-                archivo_t = secure_filename(tiempos.filename)
-                tiempos.save(os.path.join(app.config['UPLOAD_FOLDER'], archivo_t))
-                
-            if 'archivo_rutas' in request.files:
-                # Guardar archivo de rutas
-                archivo_r = secure_filename(rutas.filename)
-                rutas.save(os.path.join(app.config['UPLOAD_FOLDER'], archivo_r))
+        tiempos = request.files["archivo_tiempos"]
+        rutas = request.files["archivo_rutas"]
+    
+        if 'archivo_tiempos' in request.files:
+            # Guardar archivo de tiempos
+            archivo_t = secure_filename(tiempos.filename)
+            tiempos.save(os.path.join(app.config['UPLOAD_FOLDER'], archivo_t))
+            
+        if 'archivo_rutas' in request.files:
+            # Guardar archivo de rutas
+            archivo_r = secure_filename(rutas.filename)
+            rutas.save(os.path.join(app.config['UPLOAD_FOLDER'], archivo_r))
+    
+    elif request.method == 'GET':
+        archivo_t, archivo_r = '', ''
+
 
     return render_template("index.html", archivo_t=archivo_t, archivo_r=archivo_r)
 
@@ -76,12 +85,21 @@ def muestra_Sol():
                 Cmax, resultado = reglas.spt_rule(tiempos_proceso, rutas_produccion)
             elif regla == 'mwkr':
                 Cmax, resultado = reglas.mwkr_rule(tiempos_proceso, rutas_produccion)
+            
+            # Borrar archivos cargados y generados anteriormente
+            for document in os.listdir('./templates/'):
+                if document.startswith('gantt_'):
+                    os.remove('./templates/' + document)
+            
             gantt_chart = reglas.imprime_gantt(rutas_produccion, resultado)
             gantt_chart.write_html('./templates/gantt_'+str(Cmax)+'.html')
             return render_template('gantt_'+str(Cmax)+'.html')
+
         else:
             archivo_t, archivo_r = '', ''
             return render_template("index.html")
     
 if __name__ == '__main__':
+    scheduler.add_job(id = 'Scheduled Task', func=borar_archivos, trigger="interval", hours=24)
+    scheduler.start()
     app.run()
